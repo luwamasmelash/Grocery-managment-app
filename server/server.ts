d49e93d5e8cdfab2af276543da38123d28d30312
luwamasmelash <liwix24@gmail.com>
2026-09-03 12:20:10 +0300

    Fix Vercel Express deployment

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
-import express, { NextFunction, Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import { inngest, functions } from "./inngest/index.js";
 import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
@@ -11,25 +11,35 @@ import orderRouter from "./routes/orderRoutes.js";
 const app = express();
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+app.get("/", (req: Request, res: Response) => {
+    res.send("Server is Live!");
 });
-app.use('/api/auth', authRouter)
-app.use('/api/products', productRouter)
-app.use('/api/upload', uploadRouter)
-app.use('/api/orders', orderRouter)
-app.use("/inngest", serve({ client: inngest, functions }));
 
-app.use((error: any, req:Request, res: Response, next: NextFunction)=> {
-    console.error(error)
-    res.status(500).json({message: error.message})
-})
+app.use("/api/auth", authRouter);
+app.use("/api/products", productRouter);
+app.use("/api/upload", uploadRouter);
+app.use("/api/orders", orderRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+app.use(
+    "/inngest",
+    serve({
+        client: inngest,
+        functions,
+    })
+);
+
+// Error handler
+app.use(
+    (error: any, req: Request, res: Response, next: NextFunction) => {
+        console.error(error);
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+);
+
+// Export app for Vercel
+export default app;
